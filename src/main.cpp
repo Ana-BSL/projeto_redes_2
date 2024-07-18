cebe5f886bcba949a9f6ca885f06422138bd3783
Ana-BSL <anabeatrizna43@gmail.com>
2024-07-18 13:47:00 -0300

    Ajustando o Funcionamento do BluetoothSerial

--- src/main.cpp
+++ src/main.cpp
@@ -1,27 +1,25 @@
 #include <Arduino.h>
+//#include <WiFi.h>
 #include "BluetoothSerial.h"
-#include <WiFi.h>
-#include <PubSubClient.h>
 
-#define WIFI_SSID "UPE-Estudante(UnL)"
-#define WIFI_PASSWORD "OrgulhodeserUPE"
-#define MQTT_SERVER "broker.hivemq.com"
-#define MQTT_PORT 1883
+/*#define WIFI_SSID "Leonardo-2"
+#define WIFI_PASSWORD "lebe1204"
+#define SERVER_IP "10.0.0.164"  
+#define SERVER_PORT 12345  
 
-WiFiClient espClient;
-PubSubClient client(espClient);
+WiFiClient client;*/
 BluetoothSerial SerialBT;
 
 char dados;
 unsigned long dataMillis = 0;
 int count = 0;
 
-String getDeviceId() {
+void getDeviceId(char *deviceId) {
   uint64_t chipid = ESP.getEfuseMac();
-  String id = String((uint16_t)(chipid >> 32), HEX);
-  id += String((uint32_t)chipid, HEX);
-  id.toUpperCase();
-  return id;
+  snprintf(deviceId, 18, "%02X:%02X:%02X:%02X:%02X:%02X",
+           (uint8_t)(chipid >> 40), (uint8_t)(chipid >> 32),
+           (uint8_t)(chipid >> 24), (uint8_t)(chipid >> 16),
+           (uint8_t)(chipid >> 8), (uint8_t)chipid);
 }
 
 void setup() {
@@ -29,58 +27,66 @@ void setup() {
   SerialBT.begin("ESP32_BLUETOOTH");
   pinMode(32, OUTPUT);
   pinMode(33, OUTPUT);
-  
-  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+
+  /*WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+
   while (WiFi.status() != WL_CONNECTED) {
     delay(300);
   }
-  
-  client.setServer(MQTT_SERVER, MQTT_PORT);
-  while (!client.connected()) {
-    if (client.connect("ESP32Client")) {
-      Serial.println("Connected to MQTT Broker");
-    } else {
-      delay(5000);
-    }
-  }
-}
 
-void Conexao() {
-  if (SerialBT.connected())
-    Serial.println("connected");
-  else
-    Serial.println("disconnected");
+  if (!client.connect(SERVER_IP, SERVER_PORT)) {
+    Serial.println("Connection to server failed");
+  } else {
+    Serial.println("Connected to server");
+  }*/
 }
 
 void loop() {
-  Conexao();
+  if (SerialBT.connected()) {
+    Serial.println("Conectado via Bluetooth");
+  } else {
+    Serial.println("Desconectado");
+  }
+
   if (SerialBT.available()) {
     dados = SerialBT.read();
-    char deviceId[17]; 
-    getDeviceId().toCharArray(deviceId, 17); 
+    char deviceId[18];
+    getDeviceId(deviceId);
     
+    char msg[100];
     if (dados == 'a') {
-      digitalWrite(32, HIGH);
-      digitalWrite(33, LOW);
-      SerialBT.println("presente");
-      String msg = String("ID: ") + deviceId + ", Status: presente";
-      client.publish("esp/attendance", msg.c_str());
+      digitalWrite(33, HIGH);
+      digitalWrite(32, LOW);
+      snprintf(msg, sizeof(msg), "ID: %s, Status: presente", deviceId);
+      //client.println(msg);
+      SerialBT.println(msg);
+      Serial.println(msg);
 
     } else if (dados == 'A') {
+      digitalWrite(33, LOW);
+      digitalWrite(32, HIGH);
+      snprintf(msg, sizeof(msg), "ID: %s, Status: falta justificada", deviceId);
+      //client.println(msg);
+      SerialBT.println(msg);
+      Serial.println(msg);
+    }
+    else if (dados == 'd') {
       digitalWrite(32, LOW);
-      digitalWrite(33, HIGH);
-      SerialBT.println("falta justificada");
-      String msg = String("ID: ") + deviceId + ", Status: falta justificada";
-      client.publish("esp/attendance", msg.c_str());
+      digitalWrite(33, LOW);
+      snprintf(msg, sizeof(msg), "Desligado!");
+      //client.println(msg);
+      SerialBT.println(msg);
+      Serial.println(msg);
     }
   }
 
   if (millis() - dataMillis > 30000) { 
     dataMillis = millis();
-    String msg = String("Count: ") + count++;
-    client.publish("esp/test", msg.c_str());
+    char msg[50];
+    snprintf(msg, sizeof(msg), "Count: %d", count++);
+    //client.println(msg);
+    Serial.println(msg);  
   }
 
-  client.loop();
-  delay(100); 
+  delay(100);
 }
